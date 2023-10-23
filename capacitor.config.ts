fd6c4f248e4e972eb259dcd644bf8aeaf0eaafb3
dainiswmichel <dainis@dainis.net>
2023-10-23 02:19:47 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LeSAEp',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
